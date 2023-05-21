5c3f6d5503868861822639b6cccf6d011e17f3ce
Samira Mousaie <samira.mousaie@divar.ir>
2023-05-21 17:30:37 +0330

    footer added

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-const container = document.getElementsByClassName("container");
+const container = document.querySelector(".container")
+const searchTitle = document.querySelector(".header-bottom__searchBar-form-title input")
+const searchLocation = document.querySelector(".header-bottom__searchBar-form-location input")
+
+
 document.addEventListener("DOMContentLoaded", showitems)
+
+searchTitle.addEventListener("input", searchJobs)
+
+// searchTitle.addEventListener("input", e => {
+//     const value = e.target.value
+//     console.log(value)
+// })
+
+// searchLocation.addEventListener("input", e => {
+//     const value = e.target.value
+//     console.log(value)
+// })
+
 
 function showitems() {
     const xhr = new XMLHttpRequest()
@@ -7,39 +24,49 @@ function showitems() {
 
     xhr.onload = function() {
         if(xhr.status == 200) {
-            const users = JSON.parse(xhr.response);
-            users.forEach(user => {
-                // createUser(user)
-                console.log(user)
+            const response = JSON.parse(xhr.response);
+            response.result.items.forEach(user => {
+                createUser(user)
+                // console.log(user)
             });
         }
     }
     xhr.send()
 }
 
-// function createUser(user) {
-//     const containerCard = document.createElement("div");
-//     containerCard.classList("container-Card")
-//     containerCard.innerHTML = `
-//         <div class="container-Card__logo">
-//             <img src="${user.logo}" alt="logo">
-//         </div>
-//         <div class="container-Card__header">
-//             <p class="container-Card__header-time">${user.postedAt}</p>
-//             <p class="container-Card__header-stats">${user.contract}</p>
-//         </div>
-//         <div class="container-Card__title">
-//             <span>${user.position}</span>
-//         </div>
-//         <div class="container-Card__ComanyName">
-//             <span>${user.company}</span>
-//         </div>
-//         <div class="container-Card__location">
-//             <span>${user.location}</span>
-//         </div>
-//         <div class="container-Card__apply">
-//             <button>Apply!</button>
-//         </div>
-//     `
-//     container.appendChild(containerCard)
+function createUser(user) {
+    const containerCard = document.createElement("div");
+    containerCard.className = "container-Card"
+    containerCard.innerHTML = `
+        <div class="container-Card__logo">
+            <img src="${user.logo}" alt="logo">
+        </div>
+        <div class="container-Card__header">
+            <p class="container-Card__header-time">${user.postedAt}</p>
+            <p class="container-Card__header-stats">${user.contract}</p>
+        </div>
+        <div class="container-Card__title">
+            <span>${user.position}</span>
+        </div>
+        <div class="container-Card__ComanyName">
+            <span>${user.company}</span>
+        </div>
+        <div class="container-Card__location">
+            <span>${user.location}</span>
+        </div>
+        <div class="container-Card__apply">
+            <button>Apply!</button>
+        </div>
+    `
+    container.appendChild(containerCard)
+}
+
+
+
+// async function searchJobs() {
+//     const result = await fetch('data.json')
+//     const data = await result.json();
+//     const jobresult = data.result.items.filter(job => job.position.toLowerCase().includes(searchTitle.value))
+//     createUser(jobresult)
 // }
+
